cae3c78df3a6998664022d554e0f8628d30bf803
Tesfamariam T <51912936+Wege0921@users.noreply.github.com>
2025-07-27 23:33:00 +0300

    Update middleware.py

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -75,7 +75,7 @@ class OffensiveLanguageMiddleware:
         return request.META.get('REMOTE_ADDR')
 from django.http import HttpResponseForbidden
 
-class RolePermissionMiddleware:
+class RolepermissionMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
