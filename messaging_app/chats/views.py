67639b0baf2b0a59e7cee4169781d0db7586f7a9
Tesfamariam T <51912936+Wege0921@users.noreply.github.com>
2025-07-27 12:54:50 +0300

    Update views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -19,6 +19,24 @@ from .models import Message
 from .serializers import MessageSerializer
 from .permissions import IsOwnerOrReadOnly
 
+from .permissions import IsParticipantOfConversation
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsParticipantOfConversation]
+
+    def get_queryset(self):
+        # Limit messages to those in conversations the user is part of
+        return Message.objects.filter(conversation__participants=self.request.user)
+
+    def perform_create(self, serializer):
+        # Ensure the user is a participant in the conversation they're posting to
+        conversation = serializer.validated_data['conversation']
+        if self.request.user not in conversation.participants.all():
+            raise PermissionDenied("You are not a participant of this conversation.")
+        serializer.save(sender=self.request.user)
+
 class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
     permission_classes = [IsOwnerOrReadOnly]
