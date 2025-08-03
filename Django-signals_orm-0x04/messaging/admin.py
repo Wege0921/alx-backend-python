9c36f4682bd910eee401f79fd84321cc9f1eed2e
Tesfamariam T <51912936+Wege0921@users.noreply.github.com>
2025-08-03 22:53:25 +0300

    Update admin.py

--- Django-signals_orm-0x04/messaging/admin.py
+++ Django-signals_orm-0x04/messaging/admin.py
@@ -5,3 +5,18 @@ class MessagingConfig(AppConfig):
 
     def ready(self):
         import messaging.signals  # Ensures the signals are registered
+        
+from django.contrib import admin
+from .models import Message, Notification, MessageHistory
+
+class MessageHistoryInline(admin.TabularInline):
+    model = MessageHistory
+    extra = 0
+    readonly_fields = ('old_content', 'edited_at')
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'receiver', 'timestamp', 'edited')
+    inlines = [MessageHistoryInline]
+
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Notification)
