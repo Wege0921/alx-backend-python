89b66b7dede7d35bd92df2785a6308ffdf499ca2
Tesfamariam T <51912936+Wege0921@users.noreply.github.com>
2025-08-03 22:47:13 +0300

    Create tests.py

---
+++ Django-signals_orm-0x04/messaging/tests.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Message, Notification
+
+class MessageNotificationTestCase(TestCase):
+    def setUp(self):
+        self.sender = User.objects.create_user(username='sender', password='pass')
+        self.receiver = User.objects.create_user(username='receiver', password='pass')
+
+    def test_notification_created_on_message_send(self):
+        message = Message.objects.create(sender=self.sender, receiver=self.receiver, content="Hello")
+        notification = Notification.objects.filter(user=self.receiver, message=message)
+        self.assertTrue(notification.exists())
