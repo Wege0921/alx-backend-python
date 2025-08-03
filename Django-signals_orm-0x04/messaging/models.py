a221ad9f5826081aa2f2cc349ea481370b0f16c7
Tesfamariam T <51912936+Wege0921@users.noreply.github.com>
2025-08-03 23:05:54 +0300

    Update models.py

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+from django.contrib.auth.models import User
 
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
@@ -7,8 +9,17 @@ class Message(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     edited = models.BooleanField(default=False)
+    parent_message = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name='replies'
+    )
 
     def __str__(self):
+        if self.parent_message:
+            return f"Reply by {self.sender} to Message {self.parent_message.id}"
         return f"Message from {self.sender} to {self.receiver}"
 
 class Notification(models.Model):
@@ -27,4 +38,7 @@ class MessageHistory(models.Model):
 
     def __str__(self):
         return f"History for Message ID {self.message.id} at {self.edited_at}"
+
+
+
 
